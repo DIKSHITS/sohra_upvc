0cfa5fa1cd7fef1820e8e6a9ef7c29247f103050
Dikshit Raj <122359979+DIKSHITS@users.noreply.github.com>
2026-05-24 12:53:01 +0530

    Refactor Footer component and update contact details

--- sohraupvc/src/Components/Footer.jsx
+++ sohraupvc/src/Components/Footer.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import "./Footer.css";
 import logo from "./logo/sohra-updated.png";
 import makeInIndia from "./logo/Make_In_India.png";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapMarkerAlt, faPhone } from "@fortawesome/free-solid-svg-icons";
-import { faInstagram, faFacebookF, faWhatsapp } from "@fortawesome/free-brands-svg-icons"; // ✅ Added WhatsApp
+import {
+  faMapMarkerAlt,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faInstagram,
+  faFacebookF,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => (
   <footer className="sohra-footer">
     <div className="sohra-footer-row">
+
+      {/* Left Section */}
       <div className="footer-col left">
         <img
           src={logo}
@@ -16,31 +27,54 @@ const Footer = () => (
           alt="Sohra UPVC Industries"
         />
       </div>
+
+      {/* Center Section */}
       <div className="footer-col center">
         <div className="footer-contact left-align">
+
           <div>
-            <FontAwesomeIcon icon={faMapMarkerAlt} className="icon" />
-            Serve No. 487/2 min, Near Jaggkhedi Panchayat Bhawan, Sanjeet Road,
+            <FontAwesomeIcon
+              icon={faMapMarkerAlt}
+              className="icon"
+            />
+            Serve No. 487/2 min, Near Jaggkhedi Panchayat Bhawan,
+            Sanjeet Road
           </div>
+
           <div>
-            <FontAwesomeIcon icon={faMapMarkerAlt} className="icon" />
+            <FontAwesomeIcon
+              icon={faMapMarkerAlt}
+              className="icon"
+            />
             Village: Jaggkhedi, Dist.: Mandsaur, M.P.
           </div>
+
           <div>
-            <FontAwesomeIcon icon={faPhone} className="icon" />
+            <FontAwesomeIcon
+              icon={faPhone}
+              className="icon"
+            />
             Mobile No.: 7470955631
           </div>
+
         </div>
       </div>
+
+      {/* Right Section */}
       <div className="footer-col right">
+
         <img
           src={makeInIndia}
           className="footer-makeinindia"
           alt="Make in India"
         />
-        <br />
-        <div className="footer-social-label">Follow us on</div>
+
+        <div className="footer-social-label">
+          Follow us on
+        </div>
+
         <div className="footer-social">
+
           <a
             href="https://www.instagram.com/yourprofile"
             target="_blank"
@@ -49,6 +83,7 @@ const Footer = () => (
           >
             <FontAwesomeIcon icon={faInstagram} />
           </a>
+
           <a
             href="https://www.facebook.com/yourprofile"
             target="_blank"
@@ -57,7 +92,7 @@ const Footer = () => (
           >
             <FontAwesomeIcon icon={faFacebookF} />
           </a>
-          {/* ✅ WhatsApp link */}
+
           <a
             href="https://wa.me/7470955631"
             target="_blank"
@@ -66,12 +101,27 @@ const Footer = () => (
           >
             <FontAwesomeIcon icon={faWhatsapp} />
           </a>
+
         </div>
+
       </div>
+
     </div>
+
+    {/* Footer Bottom */}
+
     <div className="footer-copy">
-      © {new Date().getFullYear()} Sohra UPVC Industries. All Rights Reserved.
+      © {new Date().getFullYear()} Sohra UPVC Industries.
+      All Rights Reserved.
+
+      <br />
+
+      <span className="developer-credit">
+        Developed with ❤️ by{" "}
+        <strong>Dikshit Raj</strong>
+      </span>
     </div>
+
   </footer>
 );
 
